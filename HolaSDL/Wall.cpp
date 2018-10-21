b826d4ad83dd712cc634ca39a7c6673cdb0798c5
Dankgard <dangar18@ucm.es>
2018-10-21 14:34:58 +0200

    Empezado Wall

--- HolaSDL/Wall.cpp
+++ HolaSDL/Wall.cpp
@@ -1,3 +1,18 @@
 #include "Wall.h"
 #include <iostream>
 #include "checkML.h"
+
+Wall::Wall(): pos(), w(), h(), texture() {}
+
+Wall::Wall(uint w, uint h, Vector2D pos, Texture* t) :
+	w(w), h(h), pos(pos), texture(t) {}
+
+void Wall::render() const
+{
+	SDL_Rect destRect;
+	destRect.x = pos.getX();
+	destRect.y = pos.getY();
+	destRect.h = h;
+	destRect.w = w;
+	texture->render(destRect, SDL_FLIP_NONE);
+}
